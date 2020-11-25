5a59a2e52de35ae9c99caf73b484a0c82f791bbf
Takaichi Daisuke <8767154@gmail.com>
2020-11-26 02:07:20 +0900

    update directory

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 26 01:57:04 2020
+
+@author: takashi-154
+"""
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['dynamic_background_estimation.py'])
